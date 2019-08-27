bb3996c094cc267dd3cbaa253f5eb1431b6c62d4
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-08-27 16:02:18 -0400

    Update command

--- src/main/java/com/kyleharrington/ij/Main.java
+++ src/main/java/com/kyleharrington/ij/Main.java
@@ -1,7 +1,9 @@
 package com.kyleharrington.ij;
 
 
+import org.scijava.ItemIO;
 import org.scijava.command.Command;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Menu;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -13,27 +15,19 @@ import sc.iview.vector.Vector3;
 import static sc.iview.commands.MenuWeights.EDIT;
 import static sc.iview.commands.MenuWeights.EDIT_ADD_SPHERE;
 
-@Plugin(type = Command.class, menuRoot = "SciView", //
-        menu = { @Menu(label = "Edit", weight = EDIT), //
-                 @Menu(label = "Add Sphere...", weight = EDIT_ADD_SPHERE) })
+@Plugin(type = Command.class, menuRoot = "Plugins", //
+        menu = { @Menu(label = "Kharrington", weight = EDIT) })
 public class Main implements Command {
 
     @Parameter
-    private SciView sciView;
+    private LogService logService;
 
-    @Parameter
-    private String position = "0; 0; 0";
-
-    @Parameter
-    private float radius = 1.0f;
-
-    @Parameter
-    private ColorRGB color = SciView.DEFAULT_COLOR;
+    @Parameter(type = ItemIO.OUTPUT)
+    private String infoString = "Kharrington IJ update site requires: SciView, SNT";
 
     @Override
     public void run() {
-        final Vector3 pos = ClearGLVector3.parse( position );
-        sciView.addSphere( pos, radius, color );
+        logService.warn(infoString);
     }
 
     static public void main(String[] args) {
